815bfde4c9518775deec17a9dd07524b6b9e9886
Bigfish4tim <bigfish.4tim@gmail.com>
2021-12-17 10:24:27 +0900

    dynamic router test

---
+++ nuxtproject_5/pages/users/_id.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>User</h1>
+        <p>
+            User ID : {{ id }}
+        </p>
+        <p>
+            Hello : {{ hello }}
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    asyncData({params, query}) {
+        console.log(params)
+        return {
+            id: params.id,
+            hello: query.hello
+        }
+    },
+    validate({params}) {
+        return /^\d+$/.test(params.id);
+    }
+}
+</script>
+<style>
+    
+</style>
