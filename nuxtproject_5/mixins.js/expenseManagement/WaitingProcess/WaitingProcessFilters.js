2a373b0cf1114938a53d6da2501fd09c248ecedf
Bigfish4tim <bigfish.4tim@gmail.com>
2022-04-28 18:00:07 +0900

    change

---
+++ nuxtproject_5/mixins.js/expenseManagement/WaitingProcess/WaitingProcessFilters.js
@@ -0,0 +1,108 @@
+export default {
+    data() {
+        return {
+            dateFilterText: '',
+            filterDate: [],
+            statusFilterText: '',
+            speciesFilterText: '',
+            companyFilterText: '',
+            companyListText: '',
+            departmentFilterText: '',
+            bunryu2FilterText: '',
+            allFilterText: '',
+            allFilterTextSearch: '',
+            
+            //search clone data
+            dateFilterTextClone: '',
+            filterDateClone: [],
+            statusFilterTextClone: '',
+            speciesFilterTextClone: '',
+            companyFilterTextClone: '',
+            companyListTextClone: '',
+            departmentFilterTextClone: '',
+            bunryu2FilterTextClone: '',
+            allFilterTextClone: '',
+            allFilterTextSearchClone: '',
+        }
+    },
+    methods: {
+        searchEvt() {
+            console.log('click event')
+            this.dateFilterTextClone = this.dateFilterText 
+            this.filterDateClone = this.filterDate
+            this.speciesFilterTextClone = this.speciesFilterText
+            this.statusFilterTextClone = this.statusFilterText
+            this.companyFilterTextClone = this.companyFilterText 
+            this.companyListTextClone = this.companyListText 
+            this.departmentFilterTextClone = this.departmentFilterText 
+            this.bunryu2FilterTextClone = this.bunryu2FilterText 
+            this.allFilterTextClone = this.allFilterText 
+            this.allFilterTextSearchClone = this.allFilterTextSearch 
+
+        },
+        // filters
+        wiimFiltering(value) {
+            if (!this.filterDateClone) {
+                return true;
+            }
+            if (this.dateFilterTextClone === '위임일') {
+                if (value <= this.filterDateClone[1] && value >= this.filterDateClone[0]) {
+                    console.log(this.filterDateClone[0] + this.filterDateClone[1])
+                    console.log('on action')
+                    return value
+                } else {
+                    console.log('non action')
+                    // return true
+                }
+            } else {
+                return true;
+            }
+        },
+        companyFiltering(value) {
+            if(!this.companyFilterTextClone) {
+                return true
+            }
+
+            return value === this.companyFilterTextClone
+        },
+        teamfiltering(value) {
+            if(!this.departmentFilterTextClone) {
+                return true
+            }
+
+            return value === this.departmentFilterTextClone.trim()
+        },
+        itemFiltering(value) {
+
+        },
+        allFiltering(value) {
+            if(this.allFilterText === '피보험자') {
+                if(!this.allFilterText) {
+                    return true
+                }
+
+                return value === this.allFilterTextSearchClone
+            } else if(this.allFilterText === '계약자') {
+                if(!this.allFilterText) {
+                    return true
+                }
+
+                return value === this.allFilterTextSearchClone
+            } else if(this.allFilterText === '보고서번호') {
+                if(!this.allFilterText) {
+                    return true
+                }
+
+                return value === this.allFilterTextSearchClone
+            } else if(this.allFilterText === '사고번호') {
+                if(!this.allFilterText) {
+                    return true
+                }
+
+                return value === this.allFilterTextSearchClone
+            } else {
+                return true
+            }
+        },
+    },
+}
---
+++ nuxtproject_5/pages/TaskManagement/expenseManagement.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <v-btn-toggle v-model="toggle_status">
+            <v-btn @click="Rendering('WaitingProcess')">처리대기</v-btn>
+            <v-btn @click="Rendering('AmountChange')">지급대기(사원)</v-btn>
+            <v-btn @click="Rendering('ProcessComplete')">지급대기(건별)</v-btn>
+            <v-btn @click="Rendering('PerformanceChanges')">지급내역</v-btn>
+            <v-btn @click="Rendering('RateErrorList')">지급상세내역</v-btn>
+            <v-btn @click="Rendering('ProcessList')">경비집계</v-btn>
+            <v-btn @click="Rendering('ProcessList')">지급보류</v-btn>
+        </v-btn-toggle>
+        <div
+            v-if="renderCondition === 'WaitingProcess'"
+        >
+            <WaitingProcess/>
+        </div>
+        <div
+            v-if="renderCondition === 'AmountChange'"
+        >
+            <AmountChange/>
+        </div>
+        <div
+            v-if="renderCondition === 'ProcessComplete'"
+        >
+            <ProcessComplete/>
+        </div>
+        <div
+            v-if="renderCondition === 'PerformanceChanges'"
+        >
+            <PerformanceChanges/>
+        </div>
+        <div
+            v-if="renderCondition === 'RateErrorList'"
+        >
+            <RateErrorList/>
+        </div>
+        <div
+            v-if="renderCondition === 'ProcessList'"
+        >
+            <ProcessList/>
+        </div>
+    </div>
+</template>
+<script>
+import WaitingProcess from "../../components/expenseManagement/WaitingProcess.vue"
+import Unprocessed from "../../components/resultProcessing/Unprocessed.vue"
+import AmountChange from "../../components/resultProcessing/AmountChange.vue"
+import ProcessComplete from "../../components/resultProcessing/ProcessComplete.vue"
+import PerformanceChanges from "../../components/resultProcessing/PerformanceChanges.vue"
+import RateErrorList from "../../components/resultProcessing/RateErrorList.vue"
+import ProcessList from "../../components/resultProcessing/ProcessList.vue"
+
+export default {
+    components: {
+        WaitingProcess,
+        Unprocessed,
+        AmountChange,
+        ProcessComplete,
+        PerformanceChanges,
+        RateErrorList,
+        ProcessList,
+    },
+    data() {
+        return {
+            toggle_status: 0,
+
+            renderCondition: 'WaitingProcess'
+        }
+    },
+    methods: {
+        Rendering(cond) {
+            this.renderCondition = cond
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
