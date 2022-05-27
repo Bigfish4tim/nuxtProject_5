600d935133081f6148a74548ce19c2f43660f6e0
Bigfish4tim <bigfish.4tim@gmail.com>
2022-05-27 17:34:22 +0900

    announcement

---
+++ nuxtproject_5/pages/Community/announcement.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div>
+            <DeletedMemoBox/>
+        </div>
+
+    </div>
+</template>
+<script>
+import DeletedMemoBox from "../../components/MemoManagement/deletedMemoBox/DeletedMemoBox.vue"
+
+export default {
+    components: {
+        DeletedMemoBox,
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<style>
+    
+</style>
